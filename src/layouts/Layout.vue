a14dac2319b4ed54316fe6dbc53df6dc196f4e5b
pkamit <amit.pandey812@gmail.com>
2020-02-23 12:50:22 +0530

    css and drawer visibility

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,48 +1,51 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR fFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
+     <q-toolbar>
+       <!--  <q-btn
           flat
           dense
           round
           @click="leftDrawerOpen = !leftDrawerOpen"
           icon="menu"
           aria-label="Menu"
-        />
+        /> -->
 
-        <q-toolbar-title>Quasar App</q-toolbar-title>
+        <q-toolbar-title class="absolute-center">Awesome Todo</q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+     
       </q-toolbar>
     </q-header>
     <q-footer>
-      <q-tabs  >
-        <q-route-tab to="/" name="todos" icon="list" label="Todo" />
-        <q-route-tab to="/settings" name="settings" icon="settings" label="Settings" />
-       
+      <q-tabs >
+        <q-route-tab 
+         v-for= "nav in navs"  
+         :key= "nav.id" :to="nav.to" :name="nav.label" 
+         :icon="nav.icon" :label="nav.label" />
+      
       </q-tabs>
     </q-footer>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-2">
-      <q-list>
+    <q-drawer 
+    :width="250"
+    :breakpoint="767"
+    v-model="leftDrawerOpen" show-if-above bordered content-class="bg-primary">
+      <q-list dark>
         <q-item-label header>Navigations</q-item-label>
-        <q-item to="/" exact clickable>
+        <q-item 
+        v-for="nav in navs"
+        :key="nav.id" 
+        :to="nav.to"
+        class="text-grey-4"
+         exact clickable>
           <q-item-section avatar>
-            <q-icon name="list" />
+            <q-icon :name="nav.icon" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Todo</q-item-label>
+            <q-item-label>{{ nav.label}}</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item to="/Settings" exact clickable>
-          <q-item-section avatar>
-            <q-icon name="settings" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Settings</q-item-label>
-          </q-item-section>
-        </q-item>
+
       </q-list>
     </q-drawer>
 
@@ -58,8 +61,39 @@ export default {
 
   data() {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: false,
+      navs: [
+        {
+          id : 0,
+          label: "Todo",
+          icon: "list",
+          to: "/"
+        },
+        {
+          id: 1,
+          label: "Settings",
+          icon: "settings",
+          to: "/settings"
+        },
+                {
+          id: 2,
+          label: "About",
+          icon: "info",
+          to: "/about"
+        }
+      ]
     };
   }
 };
 </script>
+<style>
+@media screen and (min-width: 768px){
+  .q-footer {
+    display: none;
+  }
+} 
+.q-drawer .q-router-link--exact-active {
+  color: white!important;
+}
+
+</style>
